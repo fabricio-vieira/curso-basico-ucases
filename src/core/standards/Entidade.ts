ca151e5d4f61ba8f8e314a053249f6339d542e04
fabricio.vieira <fabricio_raldi@outlook.pt>
2026-01-28 22:23:52 -0400

    Feat: Add Entidade

---
+++ src/core/standards/Entidade.ts
@@ -0,0 +1,36 @@
+import Id from "../shared/Id"
+
+
+
+export interface EntidadeProps {
+    id?: string
+}
+
+export default abstract class Entidade<Tipo, Props extends EntidadeProps> {
+    readonly id: Id
+    readonly props: Props
+
+    constructor(props: Props) {
+        this.id = new Id(props.id)
+        this.props = { ...props, id: this.id.valor}
+    }
+
+    igual(entidade: Entidade<Tipo, Props>): boolean {
+        return this.id.igual(entidade.id)
+    }
+
+    diferente(entidade: Entidade<Tipo, Props>): boolean {
+        return !this.igual(entidade)
+    }
+
+    clone(novasProps: Props, ...args: any[]): Tipo {
+        return new (this.constructor as any)(
+            {
+                ...this.props,
+                ...novasProps
+            },
+            ...args 
+            
+        ) 
+    }
+}
